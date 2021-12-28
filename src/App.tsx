154afc8834f863a331e2e64ead2d5016ccd7362b
sohan-shingade <sohanshingade@gmail.com>
2021-12-27 19:12:29 -0800

    Added routing and charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import ContributorInput from './ContributorInput';
 import DisplayMatches from './DisplayMatches';
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom"
 
-function App() {
+function App() {  
   return (
-    <div className="App">
-      <ContributorInput />
-      <DisplayMatches />
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/contributor-input" >To ContributorInput</Link>
+        </li>
+        <li>
+          <Link to="/display-matches" >To DisplayMatches</Link>
+        </li>
+      </ul>
+
+       <Switch>
+          <Route exact path="/"><div></div></Route>
+          <Route path="/contributor-input" component={ContributorInput}/>
+          <Route path="/display-matches" component={DisplayMatches}/>
+       </Switch>
+    </Router>
   );
 }
 
--- src/DisplayMatches.tsx
+++ src/DisplayMatches.tsx
@@ -1,23 +1,29 @@
 import {FC, useEffect, useState} from 'react'
 import {db} from './firebase'
+import {ComposedChart, XAxis, YAxis, CartesianGrid, Bar} from 'recharts'
 
 const DisplayMatches:FC = () => {
     const [matchData, setMatchData] = useState<any[]>([])
     useEffect(() => {
         const fetchData = async () => {
-            const matchRef = await db.collection('regionals').doc('cc').collection('matches').get()
-            matchRef.forEach((doc) => {
-                setMatchData([...matchData, doc.id])
-            })
-            // setMatchData(matchRef.docs.map(doc => doc.data()))
+            const matchRef = await db.collection('regionals')
+                .doc('cc')
+                .collection('matches')
+                .get()
+            setMatchData(matchRef.docs.map(doc => doc.data()))
         }
         fetchData();
     }, [])
-    return (<div>
-        <ul>
-            {matchData.map(match => <li>{JSON.stringify(match)}</li>)}
-        </ul>
-    </div>)
+    return (
+        <div>
+            <ComposedChart data={matchData} width={700} height={700}>
+                <XAxis dataKey={'matchNum'}/>
+                <YAxis />
+                <CartesianGrid />
+                <Bar dataKey={'teleopBottom'} />
+            </ComposedChart>
+        </div>
+    )
 }
 
 export default DisplayMatches
